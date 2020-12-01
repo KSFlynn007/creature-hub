3d0095d1b76a2470db35aefa0c8e34886d9fdbd4
KSFlynn007 <kelseyflynnn@gmail.com>
2020-12-01 11:59:38 -0700

    Update scripts.js
    If committed, added first array containing 4 key/value pairs to use in app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
-alert('Hello world');
+let pokemonList = [
+{name: "Vulpix",
+identificationNumber: 037,
+height: 2,
+types: "fire"
+},
 
-let favoriteFood = 'Pasta'
-document.write(favoriteFood);
+{name: "Wigglytuff",
+identificationNumber: 040,
+height: 3.03,
+types: ["normal", "fairy"]
+},
+
+{name: "Oddish",
+identificationNumber: 043,
+height: 1.08,
+types: ["grass", "poison"],
+weakness: []},
+
+{name: "Wingull",
+identificationNumber: 278, 
+height: 2,
+types: ["water", "flying"]},
+
+{name: "Swinub",
+identificationNumber: 220,
+height: 1.04,
+types: ["ice", "ground"]}
+
+];
