4b3311a0161adf9175035d1334f335b3aae27271
KSFlynn007 <kelseyflynnn@gmail.com>
2020-12-02 06:37:14 -0700

    Update scripts.js
    Insert for loop with conditional based on height.

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,22 @@ height: 1.04,
 types: ["ice", "ground"]}
 
 ];
+
+//basic loop structure for list 
+
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write("<p>" + pokemonList[i].name + " - " + pokemonList[i].height + "</p>");
+// }
+
+//adds conditional to basic loop above
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >= 2 && pokemonList[i].height <= 3) {
+        document.write("<p>" + pokemonList[i].name + ", (height: " + pokemonList[i].height + " feet)" +  " - That's a regular sized pokemon. </p>");
+    } else if (pokemonList[i].height < 2) {
+        document.write("<p>" + pokemonList[i].name + ", (height: " + pokemonList[i].height + " feet)" +  " - That's a tiny pokemon! </p>");
+    } else {
+        document.write("<p>" + pokemonList[i].name + ", (height: " + pokemonList[i].height + " feet)" +  " - That's pretty big pokemon! </p>");
+
+    }
+}
