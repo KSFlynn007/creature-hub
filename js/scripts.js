5900d6f33b54e3dfc43440cbfdf2223c071ddd3b
KSFlynn007 <kelseyflynnn@gmail.com>
2020-12-21 17:12:17 -0700

    button/btn bootstrap
    adding in utility classes on <li> in JS, (<ul> in HTML already), <button> in JS, might go back to play with formatting again. re-titled appropriate button:hover and button:active CSS to reflect new class name, ".btn", rather than old ".button-class"

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,12 @@ let pokemonRepository = (function () {
     function addListItem(pokemon){
         let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
+        listItem.classList.add('group-list-item');
         let button = document.createElement('button');
         button.innerText = pokemon.name;
-        button.classList.add('button-class');
+        // DELETE LATER button.classList.add('button-class');
+        button.classList.add('btn')
+        button.classList.add('btn-secondary')
 
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);
